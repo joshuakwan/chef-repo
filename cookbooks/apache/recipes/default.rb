b054fa8e805bc1c14e0defbfe562d4209dd37006
JUN GUAN <guanjun@cn.ibm.com>
2014-07-02 14:52:55 +0800

    add my-company

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -9,29 +9,19 @@
 
 # chef-repo\cookbooks\apache\recipes\default.rb
 
-package_name = "apache2"
-service_name = "apache2"
-document_root = "/var/www"
-
-if node["platform"] == "centos"
-  package_name = "httpd"
-  service_name = "httpd"
-  document_root = "/var/www/html"
-end
-
 # install apache
-package package_name do
+package node["package_name"] do
   action :install
 end
 
 # start the apache service
 # make sure the service starts on reboot
-service service_name do
+service node["service_name"] do
   action [:enable, :start]
 end
 
 # write out home page
-template "#{document_root}/index.html" do
+template "#{node["document_root"]}/index.html" do
   source "index.html.erb"
   mode "0644"
 end
