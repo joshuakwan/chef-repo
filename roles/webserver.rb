cca227784ce066c4f0d2e8420dc8d8b6141e9cdf
JUN GUAN <guanjun@cn.ibm.com>
2014-07-03 09:58:19 +0800

    add 3 community cookbooks

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -1,6 +1,6 @@
 name "webserver"
 description "Web Servers"
-run_list "recipe[my-company]","recipe[apache]"
+run_list "recipe[my-company]","recipe[apache]","recipe[chef-client::cron]"
 default_attributes({
   "company" => "IBM"
 })
