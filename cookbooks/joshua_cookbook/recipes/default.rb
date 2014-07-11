8194c064aa1f6d227b6017c6c7d805c2fa0225e8
JUN GUAN <guanjun@cn.ibm.com>
2014-07-11 14:09:48 +0800

    add erbs

--- cookbooks/joshua_cookbook/recipes/default.rb
+++ cookbooks/joshua_cookbook/recipes/default.rb
@@ -54,3 +54,13 @@ template "/etc/logrotate.conf" do
     keep: "31"
   )
 end
+
+template "/tmp/backends.conf" do 
+  mode "0444"
+  owner "root"
+  group "root"
+  variables({
+    :enabled => true,
+    :backends => ["10.0.0.10", "10.0.0.11", "10.0.0.12"]
+  })
+end
