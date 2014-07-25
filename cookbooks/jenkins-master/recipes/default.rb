8e1315a5339d8af03692fe9b1f1ba1b35d2bfccd
JUN GUAN <guanjun@cn.ibm.com>
2014-07-25 17:02:16 +0800

    inject jjb vars

--- cookbooks/jenkins-master/recipes/default.rb
+++ cookbooks/jenkins-master/recipes/default.rb
@@ -15,8 +15,8 @@ end
 
 include_recipe "jenkins-job-builder"
 
-default['jenkins-job-builder']['user'] = 'root'
-default['jenkins-job-builder']['group'] = 'root'
-default['jenkins-job-builder']['username'] = ''
-default['jenkins-job-builder']['password'] = ''
-default['jenkins-job-builder']['url'] = 'http://127.0.0.1:8080'
+node['jenkins-job-builder']['user'] = 'root'
+node['jenkins-job-builder']['group'] = 'root'
+node['jenkins-job-builder']['username'] = ''
+node['jenkins-job-builder']['password'] = ''
+node['jenkins-job-builder']['url'] = 'http://127.0.0.1:8080'
