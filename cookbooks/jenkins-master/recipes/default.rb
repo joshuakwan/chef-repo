8b5977040b794e997cbddc62ba5d123a2add0660
JUN GUAN <guanjun@cn.ibm.com>
2014-07-29 14:32:58 +0800

    reconfig slave

--- cookbooks/jenkins-master/recipes/default.rb
+++ cookbooks/jenkins-master/recipes/default.rb
@@ -17,12 +17,26 @@ jenkins_plugin 'buildgraph-view' do
 	notifies :restart, 'service[jenkins]', :immediately
 end
 
+jenkins_password_credentials 'root' do
+  description 'Nimbus Default'
+  password    'passw0rd'
+end
+
+jenkins_ssh_slave 'chef-managed-slave' do
+  description 'This slave is managed by Chef'
+  remote_fs   '/share/executor'
+  labels      ['executor', 'nimbus', 'chef']
+  host        '9.114.194.75'
+  user        'jenkins'
+  credentials 'root'
+end
+
 include_recipe "jenkins-job-builder"
 
 cookbook_file "/usr/local/share/sample-jobs.yaml" do
 	source "sample-jobs.yaml"
 end
 
-build_jenkins_job 'sample-job' do
-	job_config "/usr/local/share/sample-jobs.yaml"
-end
+# build_jenkins_job 'sample-job' do
+# 	job_config "/usr/local/share/sample-jobs.yaml"
+# end
